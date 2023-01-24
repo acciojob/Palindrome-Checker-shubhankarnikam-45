68c8ed052c93566f7a7cda793a3972a1a34358d6
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2023-01-24 21:15:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // complete the given function
 
 function palindrome(str){
+	let flag=true;
+	str.toLowerCase();
+	let i=0;
+	let j=str.length-1;
 
+	while(i<j)
+		{
+			if(str[i]==str[j])
+			{
+				i++;
+				j--;
+			}
+			else
+			{
+				flag=false;
+				break;
+			}
+		}
+
+
+	return flag;
+	
+	
 }
 module.exports = palindrome
